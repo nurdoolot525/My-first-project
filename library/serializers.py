32eedc07f847f970490a915832b901107299461f
nurdoolot252 <nurk76452@gmail.com>
2026-07-03 13:35:47 +0300

    Исправил валидацию

--- library/serializers.py
+++ library/serializers.py
@@ -25,10 +25,9 @@ class BookSerializer(serializers.ModelSerializer):
         fields = ["id", "title", "authors", "isbn", "total_copies", "available_copies"]
 
     def validate(self, data):
-        total = data.get("total_copies", getattr(self.instance, "total_copies", None))
-        available = data.get("available_copies", getattr(self.instance, "available_copies", None))
-        if total is not None and available is not None:
-            if available < 0 or available > total:
+        total = data.get("total_copies", getattr(self.instance, "total_copies", 1))
+        available = data.get("available_copies", getattr(self.instance, "available_copies", 1))
+        if available < 0 or available > total:
                 raise serializers.ValidationError(
                     "available_copies должен быть в диапазоне от 0 до total_copies."
                 )
